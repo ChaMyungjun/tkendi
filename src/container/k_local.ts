603f37aaf0f877edbce33b47ee1c449f3976203d
Tkendi <acg70159@gmail.com>
2020-09-07 21:13:30 +0900

    position change

--- src/container/k_local.ts
+++ src/container/k_local.ts
@@ -5,14 +5,16 @@ const k_local = async (msg: any, client: any, embed: any) => {
   const setData = data.k_city;
   const field = embed;
 
-  field.addField('Create Date', data.Date[1], true);
+  field.addField('Create Date', data.Date[18]);
   for (const key in setData) {
     field.setTitle('국내 광역지방자치단체').addFields({
       name: `${Number(key) + 1}. ${setData[key]}`,
-      value: `${data.cn_name[key]} \n ${data.en_name[key]} \n 확진자: ${data.numbers.DPN[key]} \n`,
+      value: `확진자: ${data.numbers.DPN[key]} \n`,
     });
   }
   msg.channel.send(field);
 };
 
 export default k_local;
+
+// ${data.cn_name[key]} \n ${data.en_name[key]} \n 
