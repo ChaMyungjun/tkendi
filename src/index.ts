72629b43ae9c0466489ffae8825af2992d2f60e4
Tkendi <acg70159@gmail.com>
2020-09-07 15:38:17 +0900

    index change

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,25 @@
 require('dotenv').config()
-import Discord from 'discord.js';
+import Discord, { Client } from 'discord.js';
 import tkendi from './tekendi'
 import { embed } from './discord_util';
 
-const client = new Discord.Client();
-const prefix = '!';
+const PREFIX = '!';
+
+const client = new Client({disableMentions: "everyone"});
 
 client.on('guildCreate', guild => {
   console.log('Joining Server')
 })
 
+//client part
 client.on('ready', () => {
   console.log(`Bot is started, with ${client.user?.username}`)
 })
+
+client.on('warn', (info) => {
+  console.log(info)
+})
+client.on('error', console.error)
 
 client.on('message', async (msg: any) => {
   await tkendi(msg, client)
