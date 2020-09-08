a18d835f44da1a238b834f68a71c637922796a69
Tkendi <acg70159@gmail.com>
2020-09-08 11:19:56 +0900

    parsing time change

--- src/api/data.ts
+++ src/api/data.ts
@@ -11,7 +11,7 @@ let endDt = `${date.getFullYear()}${('0' + (date.getMonth() + 1)).slice(-2)}${(
   '0' + date.getDate()
 ).slice(-2)}`;
 
-if (date.getHours() < 12) {
+if (date.getHours() < 10) {
   startDt = `${date.getFullYear()}${('0' + (date.getMonth() + 1)).slice(-2)}${(
     '0' +
     (date.getDate() - 1)
