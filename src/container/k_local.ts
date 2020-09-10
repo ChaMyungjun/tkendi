ebf85f5f7c33482643d5ad9ce6e228154733347f
Tkendi <acg70159@gmail.com>
2020-09-10 16:07:36 +0900

    test awaitMessages

--- src/container/k_local.ts
+++ src/container/k_local.ts
@@ -5,26 +5,31 @@ const k_local = async (msg: any, client: any, embed: any) => {
   const setData = data.k_city;
   const field = embed;
 
-  const filter = ((res: any) => {
-    return console.log(res.content)
-  })
+  const filter = (res: any) => {
+    console.log(res.content);
+    return res.content;
+  };
 
-  field.addField('Create Date', data.Date[18]);
-  for (const key in setData) {
-    field.setTitle('국내 광역지방자치단체').addFields({
-      name: `${Number(key) + 1}. ${setData[key]}`,
-      value: `확진자: ${data.numbers.DPN[key]} \n`,
-    });
-  }
+  // field.addField('Create Date', data.Date[0]);
 
-  msg.channel.send(field).then(() => {
+  // const k_city_picker = msg.channel.send('국내 광역지방자치 단체를 입력해줘!')
+
+  // for (const key in setData) {
+  //   field.setTitle('국내 광역지방자치단체').addFields({
+  //     name: `${Number(key) + 1}. ${setData[key]}`,
+  //     value: `확진자: ${data.numbers.DPN[key]} \n`,
+  //   });
+  // }
+
+  msg.channel.send('국내 광역지방자치 단체를 입력해줘').then(() => {
     msg.channel
       .awaitMessages(filter, { max: 1, time: 6000, error: ['time'] })
       .then((collected: any) => {
+        console.log(collected.content)
         msg.channel.send(`${collected}`);
       })
       .catch((error: any) => {
-        msg.channel.send('Erorr!');
+        msg.reply('Time out!');
       });
   });
 };
